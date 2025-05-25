2fbd74adf6cae069e112defe9f220d3b367ebb36
fellenbrais1 <106751977+fellenbrais1@users.noreply.github.com>
2025-05-26 00:36:17 +0900

    asadaDADAD

--- scripts/firebaseConfig.js
+++ scripts/firebaseConfig.js
@@ -59,17 +59,19 @@ function confirmFirebaseInitialization() {
 
 // Function to get the initialized Firebase variables for use in other client-side modules
 export async function getFirebaseVariables() {
-  if (setUpResult === true) {
-    const firebaseVariables = {
-      FIREBASEAPP: firebaseApp,
-      ANALYTICS: analytics,
-      DATABASE: database,
-    };
-    return firebaseVariables;
-  } else {
-    console.warn("Firebase not successfully set up on the client-side yet");
-    return null;
-  }
+  setTimeout(() => {
+    if (setUpResult === true) {
+      const firebaseVariables = {
+        FIREBASEAPP: firebaseApp,
+        ANALYTICS: analytics,
+        DATABASE: database,
+      };
+      return firebaseVariables;
+    } else {
+      console.warn("Firebase not successfully set up on the client-side yet");
+      return null;
+    }
+  }, 1000);
 }
 
 // --- Main execution logic for your client-side app ---
